436d988af2fd431293888c6ba367f426e5df8f77
Bloaiza1313 <bloaizatovar@csumb.edu>
2026-03-24 16:54:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import qrcode
-from PIL import Image  # allows us to use Image functions from Pillow library
+from PIL import Image
 
-data = st.text_input("Enter text or URL", "Python is fun")  # creates an input box; default text is "Python is fun"
-img = qrcode.make(data)  # generate QR code from whatever user enters
-img.save("MyQRCode1.jpg")  # save QR code as image file
-img = Image.open("MyQRCode1.jpg")  # open the saved QR code image
-st.image(img)  # display the QR code in the Streamlit web app
+data = st.text_input("Enter text or URL", "Python is fun")
+img = qrcode.make(data)
+img.save("MyQRCode1.jpg")
+img = Image.open("MyQRCode1.jpg")
+st.image(img)
